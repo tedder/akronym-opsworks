109cd612ecf4dd60652e86778c5884dc7211f54e
tedder <ted@adhoclabs.co>
2013-09-11 19:57:26 -0700

    first commits

---
+++ erlang-runner/recipes/default.rb
@@ -0,0 +1,15 @@
+#
+# Cookbook Name:: erlang-runner
+# Recipe:: default
+#
+# Copyright 2013, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+package 'mosh'
+package 'python'
+package 'libssl0.9.8'
+package 'erlang'
+
+#python/pip tips
+#https://forums.aws.amazon.com/thread.jspa?messageID=468626
