f55912c65ef3195211a02fe481641154a704ab0a
tedder <ted@adhoclabs.co>
2013-10-20 20:19:07 -0700

    redis, mostly for the cli

--- erlang-runner/recipes/default.rb
+++ erlang-runner/recipes/default.rb
@@ -11,7 +11,7 @@ package 'mosh'
 package 'libssl0.9.8'
 package 'erlang'
 #package 'libdigest-hmac-perl' # this is hacky, but needed for scm_helper in opsworks/s3curl.pl
-
+package 'redis-server'
 
 #python/pip tips
 #https://forums.aws.amazon.com/thread.jspa?messageID=468626
